60a0b27f39cbc0ac62fb777acb019594f691d424
Mimic <mimic@luna.town>
2021-05-08 16:34:42 -0400

    Add a lock for generation/response. While definitely not the best way to do this, multiple users can (theoretically) use the program simultaneously now.

--- webserver.py
+++ webserver.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify, render_template, cli, Response
 pipe = None # The multiprocessing pipe, sent by the main.py process starter
 app = Flask(__name__, static_url_path='/')
 
+import threading
+mainlock = threading.Lock()
+
 #cli.show_server_banner = lambda *_: None
 
 @app.route('/')
@@ -11,5 +14,9 @@ def index():
 @app.route('/gen', methods=['POST'])
 def gen():
     #print("/gen receives:", type(request.json), request.json)
+
+    mainlock.acquire()
     pipe.send(request.json)
-    return pipe.recv()
+    r = pipe.recv()
+    mainlock.release()
+    return r
